6c72b629aa3432368a34430c89fae601e29c53dc
Evgeniy Zuykin <e.zuykin@2gis.ru>
2019-05-30 20:02:06 +0700

    Command line interface

--- build.sbt
+++ build.sbt
@@ -10,7 +10,7 @@ lazy val root = (project in file(".")).
     libraryDependencies ++= Seq(
       "com.squareup.okhttp3" % "okhttp" % "3.14.2",
       "commons-net" % "commons-net" % "3.6",
-
+      
       "ch.qos.logback" % "logback-classic" % "1.2.3",
 
       "org.scalatest" %% "scalatest" % "3.0.5" % Test
--- src/main/scala/com/github/shaad/filedownloader/Main.scala
+++ src/main/scala/com/github/shaad/filedownloader/Main.scala
@@ -5,49 +5,56 @@ import java.net.URI
 import java.nio.file.Paths
 import java.util.concurrent.Executors
 
-import com.github.shaad.filedownloader.downloader.{ FtpFileDownloader, HttpFileDownloader }
+import com.github.shaad.filedownloader.downloader.{FtpFileDownloader, HttpFileDownloader}
 
-import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.{ Failure, Success }
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object Main extends App with WithLogger {
-  val tempDir = "/tmp/downloads/temp"
-  val resDir = "/tmp/downloads/res"
+  val usage =
+    """Usage:
+      |java -jar file_downloader.jar --temp-dir=absolute_path --result-dir=absolute_path urls
+    """.stripMargin
+
+  val tempDir = args.filter(_.startsWith("--temp-dir=")) match {
+    case r if r.length != 1 =>
+      println(usage)
+      System.exit(1).asInstanceOf[String]
+    case r => r(0).split('=').last
+  }
+
+  val resDir = args.filter(_.startsWith("--result-dir=")) match {
+    case r if r.length != 1 =>
+      println(usage)
+      System.exit(1).asInstanceOf[String]
+    case r => r(0).split('=').last
+  }
 
   new File(tempDir).mkdirs()
   new File(resDir).mkdirs()
 
-  val fileURLs =
-    List(
-      //      "http://www.clker.com/cliparts/0/f/d/b/12917289761851255679earth-map-huge-md.png",
-      "http://lhcb-reconstruction.web.cern.ch/lhcb-reconstruction/Augenblick/Pictures/72252_71060817_0_huge.jpg",
-      "ftp://speedtest.tele2.net/20MB.zip",
-      //      "ftp://speedtest.tele2.net/200MB.zip",
-      "ftp://speedtest.tele2.net/500MB.zip",
-      "ftp://speedtest:speedtest@ftp.otenet.gr/test10Mb.db",
-      "http://www.ovh.net/files/100Mio.dat",
-      //      "http://www.ovh.net/files/1Gio.dat",
-      //      "https://speed.hetzner.de/10GB.bin",
-      //      "https://speed.hetzner.de/1GB.bin",
-      "https://speed.hetzner.de/100MB.bin")
-
   implicit val context = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors() * 4))
 
-  val futures = fileURLs
-    .map(new URI(_))
-    .map(uri => {
-      val fileName = uri.getPath.split("/").last
+  val futures: List[Future[FileDownloadResult]] =
+    args
+      .filter(a => !a.startsWith("--"))
+      .toList
+      .map(new URI(_))
+      .map(uri => {
+        val fileName = uri.getPath.split("/").last
 
-      val tempFile = Paths.get(tempDir, fileName)
-      val resFile = Paths.get(resDir, fileName)
+        val tempFile = Paths.get(tempDir, fileName)
+        val resFile = Paths.get(resDir, fileName)
 
-      uri.getScheme match {
-        case "http" | "https" => new HttpFileDownloader().download(uri, tempFile, resFile)
-        case "ftp" | "ftps" => new FtpFileDownloader().download(uri, tempFile, resFile)
-        case _ => Future(ProtocolNotSupported(uri.getScheme))
-        // extend protocols here
-      }
-    })
+        uri.getScheme match {
+          case "http" | "https" => new HttpFileDownloader().download(uri, tempFile, resFile)
+          case "ftp" | "ftps" => new FtpFileDownloader().download(uri, tempFile, resFile)
+          case _ =>
+            log.error(s"Schema not supported ${uri.getScheme}")
+            Future(DownloadFailed(ProtocolNotSupported(uri.getScheme)))
+          // extend protocols here
+        }
+      })
 
   Future.sequence(futures)
     .onComplete {
